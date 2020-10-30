56d435d97150b20752697ea9c1cbdad957f6ff42
oereo <dlstpgns0406@gmail.com>
2020-10-30 11:10:32 +0900

    [ADD] develop

--- sns/settings.py
+++ sns/settings.py
@@ -1,10 +1,8 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'dq_s@_7zbz4k4ws=_(&_asg#*!_gsyrl%(lr!ky8rsh$&1n6ew'

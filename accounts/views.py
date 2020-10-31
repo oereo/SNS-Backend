d9abe759fd12eb6756d8d6db4117db0b96049b52
oereo <dlstpgns0406@gmail.com>
2020-10-31 18:15:34 +0900

    [FIX] comments

--- accounts/views.py
+++ accounts/views.py
@@ -8,40 +8,6 @@ def login(request):
     return render(request, "login.html")
 
 
-# def kakao_callback(request):
-#     app_rest_api_key = 'd01f306c90eda69601dda10cdf62631e'
-#     redirect_uri = "http://localhost:8000/oauth"
-#     user_token = request.GET.get("code")
-#
-#
-#     token_request = requests.post(
-#         f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={app_rest_api_key}&redirect_uri={redirect_uri}&code={user_token}"
-#     )
-#
-#     token_response_json = token_request.json()
-#     print(token_response_json)
-#     access_token = token_response_json.get("access_token")
-#     print(access_token)
-#
-#     profile_request = requests.post(
-#         f"https://kapi.kakao.com/v2/user/me",
-#         headers={"Authorization": f"Bearer {access_token}"},
-#     )
-#     profile_json = profile_request.json()
-#     print(profile_json)
-#     kakao_account = profile_json.get("kakao_account")
-#     email = kakao_account.get("email", None)
-#     profile = kakao_account.get("profile")
-#     nickname = profile.get("nickname")
-#     profile_image = profile.get("thumbnail_image_url")
-#
-#     user, _ = User.objects.get_or_create(email=email)
-#     SocialAccount.objects.get_or_create(
-#         user=user, provider="kakao", uid=user.id
-#     )
-#     return redirect("profile_register")
-
-
 def profile_register(request):
     code = request.GET['code']
     grant_type = 'authorization_code'
@@ -55,28 +21,37 @@ def profile_register(request):
     }
 
     url = 'https://kauth.kakao.com/oauth/token'
-    r = requests.post(url, data=param)
-    json_result = r.json()
-    print(r.json())
+    kakao_response = requests.post(url, data=param)
+    json_kakao_response = kakao_response.json()
+
+    print(kakao_response.json())
     print("--------------------------------")
-    access_token = json_result['access_token']
+
+    access_token = json_kakao_response['access_token']
     profile_request = requests.post(
         f"https://kapi.kakao.com/v2/user/me",
         headers={"Authorization": f"Bearer {access_token}"},
     )
+
     profile_json = profile_request.json()
     print(profile_json)
+
     kakao_account = profile_json.get("kakao_account")
+
+    # 필요한 정보들 알아서 가져오기!
     email = kakao_account.get("email")
     kakao_id = profile_json.get("id")
     profile = kakao_account.get("profile")
     nickname = profile.get("nickname")
     profile_image = profile.get("thumbnail_image_url")
-    print(kakao_id)
+
+    # 정보를 토대로 유저 생성
     user, _ = User.objects.get_or_create(email=email)
     SocialAccount.objects.get_or_create(
         user=user, provider="kakao", uid=kakao_id
     )
+
+    # 정보를 토대로 프로필 생성
     Profile.objects.get_or_create(user=user, nick=nickname)
 
     return render(request, "profile.html", {'access_token': access_token})

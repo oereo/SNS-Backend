80cc6147c6faed685f6b914126f3d005178ab00a
oereo <dlstpgns0406@gmail.com>
2020-11-06 17:50:52 +0900

    [ADD] postpage

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('main/', main, name="main"),
     path('generic/', generic, name="generic"),
-    path('mypage/', mypage, name="mypage")
+    path('mypage/', mypage, name="mypage"),
+    path('postpage', postpage, name="postpage")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from accounts.models import Profile
+from .models import Blog
+from django.utils import timezone
+
 
 
 def main(request):
@@ -15,3 +18,20 @@ def mypage(request):
     profile = Profile.objects.get(user=user)
     print(profile)
     return render(request, 'mypage.html')
+
+
+def postpage(request):
+    return render(request, 'postpage.html')
+
+
+def create(request):
+    user = request.user
+    blog = Blog()
+    blog.user_id = user.id
+    blog.owner = user.username
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    # return redirect('/blog/'+str(blog.id))
+    return redirect('main')
